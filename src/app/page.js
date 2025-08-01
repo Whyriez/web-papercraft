8d14ecb2023c9d20f68f14d9768c655374468d26
Alim Suma <sumaalim@gmail.com>
2025-08-01 14:12:12 +0800

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-'use client'
+"use client";
 import Navbar from "@/components/Navbar";
+import { Suspense } from "react";
 import Sidebar from "@/components/Sidebar";
 import PapercraftGrid from "@/components/PapercraftGrid";
 import Footer from "@/components/Footer";
@@ -14,7 +15,9 @@ export default function HomePage() {
       <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row">
         <Sidebar />
         <main className="flex-1">
-          <PapercraftGrid />
+          <Suspense fallback={<div>Loading crafts...</div>}>
+            <PapercraftGrid />
+          </Suspense>
         </main>
       </div>
       <Footer />
